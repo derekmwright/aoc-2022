5ff6afede700c180e72ab67208495036b314610e
Derek Wright <dwright@eitccorp.com>
2022-12-05 17:24:43 -0500

    day 5 part 1

--- day5/main.go
+++ day5/main.go
@@ -1,35 +1,215 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
-// [A]     [B]
-// [C] [D] [E]
-//  1   2   3
+// // Stack Diagram
 //
-// If read from top to bottom
+//	[A]
+//	[B] [C]
+//	[D] [E] [F]
+//	 1   2   3
 //
-// stacks = [['A','C'],['','D'],['B','E']]
+// move 1 from 2 to 3
+//
+// Rows give us how many stacks we should have, however shorter rows are not padded to match full length rows.
+// We need to keep track of how many crates per row we scan in account for gaps between stacks.
+//
+// # Crate identifiers are single character
+//
+// # If read from top to bottom
+//
+// stacks = [['A','C'],[”,'D'],['B','E']]
 //
 // from 1 move 1 to 2
 //
 // stacks = [['C'],['A','D']['B','E']]
-
 const (
-	SCRATE = '['
-	ECRATE = ']'
-	SPACE  = ' '
+	SCRATE  = '['
+	ECRATE  = ']'
+	SPACE   = ' '
+	NEWLINE = '\n'
+	INSTART = 'm' // instructions start with a lowercase m
 )
 
-func ScanCrates(in string) {
-	collect := []byte{}
-	for _, r := range in {
-		switch r {
-		case SCRATE:
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+//
+// Stack tracking
+//
+
+type Stack []byte
+
+func NewStack() Stack {
+	return make(Stack, 0)
+}
+
+func (s *Stack) addCrate(ident byte) {
+	*s = append(*s, ident)
+}
+
+type Stacks []Stack
+
+func (s *Stacks) move(num, src, dst int) {
+	for i := 0; i < num; i++ {
+		(*s)[dst] = append((*s)[dst], (*s)[src][len((*s)[src])-1])
+		(*s)[src] = (*s)[src][:len((*s)[src])-1]
+	}
+}
+
+type Instruction struct {
+	NumItems    int
+	Source      int
+	Destination int
+}
+
+func NewInstruction(num, src, dst int) *Instruction {
+	return &Instruction{num, src, dst}
+}
+
+type Inventory struct {
+	Stacks       Stacks
+	Instructions []Instruction
+}
+
+func (i *Inventory) Execute() string {
+	// Reverse the stacks so that we always move from the top (end of the slice), since we read from top to bottom.
+	for _, s := range i.Stacks {
+		for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+			s[i], s[j] = s[j], s[i]
 		}
 	}
-	fmt.Println(in)
+
+	for _, inst := range i.Instructions {
+		i.Stacks.move(inst.NumItems, inst.Source, inst.Destination)
+	}
+
+	result := &strings.Builder{}
+
+	for _, s := range i.Stacks {
+		if len(s) > 0 {
+			result.WriteByte(s[len(s)-1])
+		}
+	}
+
+	return result.String()
+}
+
+// scanCrate returns the byte identifier for a crate, this may be a byte value that representing an ASCII character or 0 which indicates empty.
+func (l *Lexer) scanCrate() byte {
+	if l.ch == SCRATE {
+		l.readChar() // we already know the crate started, move to identifier
+		b := l.ch
+		l.readChar()
+		return b
+	}
+	// We are going to assume the Row Scanner picked up a space and we are going to return empty crate
+	l.readPosition += 2
+	return 0
+}
+
+// ScanLine scans a line out of the lexer and returns the stack that was parsed.
+func (l *Lexer) Scan() *Inventory {
+	s := make(Stacks, 10) // feels a lil cheaty but looking at the input we dont have more than 9 rows
+	st := 0
+	eof := false
+	inst := make([]Instruction, 0)
+
+	for {
+		switch l.ch {
+		case SCRATE:
+			s[st].addCrate(l.scanCrate())
+			st += 1
+		case SPACE:
+			if l.peekChar() == SPACE {
+				// s[st].addCrate(0)
+				st += 1
+				l.readPosition += 3
+			}
+
+			// skip junk line with stack numbers
+			if st == 0 {
+				if l.peekChar() >= 48 && l.peekChar() <= 57 {
+					l.readPosition += strings.IndexByte(l.input[l.position:], byte('\n'))
+				}
+			}
+		case NEWLINE:
+			st = 0
+		case INSTART:
+			var inbuf *bytes.Buffer
+			readN := strings.IndexByte(l.input[l.position:], byte('\n'))
+			if readN == -1 {
+				inbuf = bytes.NewBuffer([]byte(l.input[l.position:]))
+				l.readPosition = len(l.input)
+			} else {
+				inbuf = bytes.NewBuffer([]byte(l.input[l.position : l.position+readN]))
+				l.readPosition += readN
+			}
+
+			n, s, d := 0, 0, 0
+			fmt.Fscanf(inbuf, "move %d from %d to %d\n", &n, &s, &d)
+			inst = append(inst, *NewInstruction(n, s-1, d-1))
+		case 0:
+			if len(s[st]) != 0 {
+				st = 0
+			}
+			eof = true
+		}
+
+		if eof {
+			break
+		}
+
+		l.readChar()
+	}
+
+	return &Inventory{
+		Stacks:       s,
+		Instructions: inst,
+	}
 }
 
 func main() {
+	f, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	l := NewLexer(string(f))
+	inv := l.Scan()
+	fmt.Println(inv.Execute())
 }
--- day5/main_test.go
+++ day5/main_test.go
@@ -1,8 +1,47 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestLexer(t *testing.T) {
-	input := "[A] [B]    [D]"
-	ScanCrates(input)
+	tests := []struct {
+		name string
+		have string
+		want []byte
+	}{
+		{
+			name: "check for empty crate in row",
+			have: "[A] [B]    [D]\n 1  2  3  4 \n\nmove 1 from 1 to 2\nmove 2 from 2 to 1",
+			want: []byte{65, 66, 0, 68},
+		},
+		// {
+		// 	name: "single crate",
+		// 	have: "[A]",
+		// 	want: []byte{65},
+		// },
+		// {
+		// 	name: "check for empty crate at start and crate at end",
+		// 	have: "    [A]",
+		// 	want: []byte{0, 65},
+		// },
+		// {
+		// 	name: "check ordering of ints doesnt change",
+		// 	have: "[B] [A] [D] [C]",
+		// 	want: []byte{66, 65, 68, 67},
+		// },
+		// {
+		// 	name: "check for multiple lines",
+		// 	have: "[B] [A]    [D]\n[C] [E] [F] [G]",
+		// 	want: []byte{65, 66, 0, 68},
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := NewLexer(tt.have)
+			got := l.Scan()
+			fmt.Printf("%+v\n", got.Execute())
+		})
+	}
 }
