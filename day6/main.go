f354d67cb3b1bdbc83c3c2616e55a8d85ab6e517
Derek Wright <dwright@eitccorp.com>
2022-12-06 10:28:54 -0500

    little bit more restructuring

--- day6/main.go
+++ day6/main.go
@@ -6,13 +6,34 @@ import (
 	"os"
 )
 
+type Window map[byte]int
+
+func (w Window) add(b byte) {
+	if _, ok := w[b]; ok {
+		w[b] += 1
+		return
+	}
+	w[b] = 1
+}
+
+func (w Window) delete(b byte) {
+	if i, ok := w[b]; ok {
+		if i-1 <= 0 {
+			delete(w, b)
+			return
+		}
+		w[b] -= 1
+	}
+}
+
 type Scanner struct {
 	input    []byte
 	position int
+	window   Window
 }
 
 func NewScanner(b []byte) Scanner {
-	return Scanner{b, 0}
+	return Scanner{b, 0, make(Window, 0)}
 }
 
 // Scan finds a series of unique characters within the specified size.
@@ -34,34 +55,12 @@ func (s Scanner) Scan(size int) int {
 	return -1
 }
 
-type Window map[byte]int
-
-func (w Window) add(b byte) {
-	if _, ok := w[b]; ok {
-		w[b] += 1
-		return
-	}
-	w[b] = 1
-}
-
-func (w Window) delete(b byte) {
-	if i, ok := w[b]; ok {
-		if i-1 <= 0 {
-			delete(w, b)
-			return
-		}
-		w[b] -= 1
-	}
-}
-
 func (s Scanner) ScanNew(size int) int {
-	w := make(Window, 0)
-
 	for s.position < (len(s.input) - size) {
-		w.add(s.input[s.position])
+		s.window.add(s.input[s.position])
 		if s.position >= size {
-			w.delete(s.input[s.position-size])
-			if len(w) == size {
+			s.window.delete(s.input[s.position-size])
+			if len(s.window) == size {
 				return s.position + 1
 			}
 		}
